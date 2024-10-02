519c88e936b28e51b655804e4a485964dd60472e
matt-r-owen <matohwhen@gmail.com>
2024-10-02 11:04:17 -0700

    project complete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
+const { message1, message2, message3 } = require('./messages/index');
 
-// Your code here
+const msg1 = message1;
+const msg2 = message2;
+const msg3 = message3;
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
